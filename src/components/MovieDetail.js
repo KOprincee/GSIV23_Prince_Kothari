6c83ae9636f50668ae57a8150d0673c988ca1166
Prince Kothari <princemkothari@gmail.com>
2023-08-16 15:38:26 +0530

    Minor fixes

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,17 +1,51 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import "./MovieDetail.css";
 
 const MovieDetail = () => {
+  const [movie, setMovie] = useState([]);
+  let { movieId } = useParams();
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?append_to_response=credits`,
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOWJjMGM5OTY5YWRkNDg4Zjc3YTNiMGIxODMyNTQ1MiIsInN1YiI6IjY0ZGE0YjBhMzcxMDk3MDBhYzQyNjNmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EpCzZ3TglQW4v3fcJkv31NF5cjiNdkaRb2np8bgn3Lg",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((movieData) => {
+        setMovie(movieData);
+      });
+  }, [movieId]);
+
+  console.log(movie);
+
   return (
     <div className="movie-detail">
-      <div className="movie-poster">No Image available</div>
+      {movie.poster_path ? (
+        <div
+          className="movie-poster"
+          style={{
+            backgroundImage: `url("https://image.tmdb.org/t/p/w200/${movie.poster_path}")`,
+          }}
+        ></div>
+      ) : (
+        <div className="movie-poster">No Image Available</div>
+      )}
+
       <div className="movie-info">
         <div className="movie-header">
-          <span className="detail-title">Shawshank </span>
-          <span className="detail-rating">4.5</span>
+          <span className="detail-title">{movie.title} </span>
+          <span className="detail-rating">{movie.vote_average}</span>
         </div>
         <div className="detail-year-len-dir">Year | Length | Director</div>
-        <div className="detail-cast">Cast: Actor 1, Actor 2, Actor 3</div>
-        <div className="detail-description">Description: Lorem Ipsum</div>
+        <div className="detail-cast">Cast:</div>
+        <div className="detail-description">Description: {movie.overview}</div>
       </div>
     </div>
   );
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,30 +1,25 @@
 import React, { useEffect, useState } from "react";
 import ListHeader from "../components/ListHeader";
 import MovieCard from "../components/MovieCard";
-import getDates from "../util/getDates";
 import "./List.css";
 
 const List = () => {
   const [movieData, setMovieData] = useState([]);
-  const { todayDateFormatted, futureDateFormatted } = getDates();
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?page=1&primary_release_date.gte=${todayDateFormatted}&primary_release_date.lte=${futureDateFormatted}&sort_by=primary_release_date.asc`,
-      {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOWJjMGM5OTY5YWRkNDg4Zjc3YTNiMGIxODMyNTQ1MiIsInN1YiI6IjY0ZGE0YjBhMzcxMDk3MDBhYzQyNjNmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EpCzZ3TglQW4v3fcJkv31NF5cjiNdkaRb2np8bgn3Lg",
-        },
-      }
-    )
+    fetch(`https://api.themoviedb.org/3/movie/upcoming`, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOWJjMGM5OTY5YWRkNDg4Zjc3YTNiMGIxODMyNTQ1MiIsInN1YiI6IjY0ZGE0YjBhMzcxMDk3MDBhYzQyNjNmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EpCzZ3TglQW4v3fcJkv31NF5cjiNdkaRb2np8bgn3Lg",
+      },
+    })
       .then((response) => response.json())
       .then((movieData) => {
         setMovieData(movieData.results);
       });
-  }, [todayDateFormatted, futureDateFormatted]);
+  }, []);
 
   return (
     <>
